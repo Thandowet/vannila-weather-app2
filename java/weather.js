0328925436ae7e7f7b180ed57ef3945e4fb95ac9
Thandowet <161431477+Thandowet@users.noreply.github.com>
2024-03-28 10:04:02 +0200

    made correction on js

--- java/weather.js
+++ java/weather.js
@@ -1,26 +1,45 @@
 function updateCurrentData(response) {
   let temperatureElement = document.querySelector("#temperature-degree");
-  let temperatureDegree = response.data.temperature.current;
-
-  temperatureElement.innerHTML = Math.round(temperatureDegree);
-}
-
-function searchCity(city) {
-  let apiKey = "3267c26b5357o136a40fatcb1530573a";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(updateCurrentData);
+  let temperatureDegree = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperatureDegree;
 }
 
 function search(event) {
   event.preventDefault();
 
   let searchInputElement = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInputElement.value;
-  searchCity(searchInputElement.value);
+  let city = searchInputElement.value;
+
+  let apiKey = "3267c26b5357o136a40fatcb1530573a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateCurrentData);
 }
+
+function formatDate(date) {
+  let days = date.getDay();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+}
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+let formatDate = days[days];
+return `${formatDate} ${hours}:${minutes}`;
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", search);
 
-searchCity(Durban);
+let applicationDetailsElement = document.querySelector("#application-details");
+let applicationDetails = new Date();
+
+applicationDetailsElement.innerHTML = formatDate(applicationDetails);
+
