fd15bb104bb397c4a51964d4dd21def7a4536fc7
Thandowet <161431477+Thandowet@users.noreply.github.com>
2024-04-01 11:22:30 +0200

    made chages on html, injected js

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ function displayTemperature(response) {
   let condtionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
-  let iconElement=document.querySelector("#icon");
+  let iconElement = document.querySelector("#icon");
 
-  iconElement.innerHTML=`<img src="${response.data.condition.icon_url}" class="temperature-emoji"/>`;
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
   condtionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="temperature-emoji"/>`;
 }
 
 function search(event) {
